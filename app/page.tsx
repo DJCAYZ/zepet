1e4ad1094283c65cf8583cae528228c04de4682e
DJCAYZ <charlesaaron07@gmail.com>
2026-03-06 15:48:26 +0800

    preload for better performance

--- app/page.tsx
+++ app/page.tsx
@@ -17,13 +17,14 @@ export default function Home() {
     .second(0)
     .millisecond(0);
 
-  const [timeMode, setTimeMode] = useState<'zepet' | 'ph_time'>('zepet');
+  const [timeMode, setTimeMode] = useState<'zepet' | 'ph_time'>('ph_time');
   const [time, setTime] = useState(dayjs());
 
   // whether user is hovering over the image
   const [hovered, setHovered] = useState(false);
 
   useEffect(() => {
+    // keep the clock updated
     function updateTime() {
       setTime(dayjs())
     }
@@ -35,6 +36,12 @@ export default function Home() {
     }
   }, [])
 
+  // preload hover gif to avoid delay
+  useEffect(() => {
+    const pre = new Image();
+    pre.src = '/gifs/chipe.gif';
+  }, []);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-7xl flex-col gap-4 items-center justify-center py-32 px-16 bg-white dark:bg-black">
@@ -42,12 +49,12 @@ export default function Home() {
           className={
             `${hovered ? 'rounded-none' : 'rounded-full'}
             aspect-square object-cover object-top
-            w-[360px] h-[360px]`
+            w-90 h-90`
           }
           alt='zepe'
           src={hovered ? '/gifs/chipe.gif' : '/images/chipe.jpg'}
-          width={240}
-          height={240}
+          width={360}
+          height={360}
           onMouseEnter={() => setHovered(true)}
           onMouseLeave={() => setHovered(false)}
         />
